77ffa7a914bc437f72c7f948b6ac82465bde2f98
Stefan Berg <isbeorn86@googlemail.com>
2023-08-14 04:55:45 +0200

    Added more examples for device driver and dockables

---
+++ NINA.Plugin.Template/MyPluginDockables/MyPluginDockableTemplates.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace $pluginnamespace$.$pluginclassname$Dockables {
+    [Export(typeof(ResourceDictionary))]
+    public partial class MyPluginDockableTemplates : ResourceDictionary {
+        public MyPluginDockableTemplates() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ NINA.Plugin.Template/MyPluginDrivers/MyPluginWeatherProvider.cs
@@ -0,0 +1,37 @@
+﻿using NINA.Core.Utility;
+using NINA.Equipment.Interfaces.ViewModel;
+using NINA.Equipment.Interfaces;
+using NINA.Profile.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+
+namespace $pluginnamespace$.$pluginclassname$Drivers {
+    /// <summary>
+    /// This Class shows the basic principle on how to add a new Device driver to N.I.N.A. via the plugin interface
+    /// When the application scans for equipment the "GetEquipment" method of a device provider is called.
+    /// This method should then return the specific List of Devices that you can connect to
+    /// </summary>
+    [Export(typeof(IEquipmentProvider))]
+    public class $pluginclassname$WeatherProvider : IEquipmentProvider<IWeatherData> {
+        private IProfileService profileService;
+
+        [ImportingConstructor]
+        public $pluginclassname$WeatherProvider(IProfileService profileService) {
+            this.profileService = profileService;
+        }
+
+        public string Name => "$pluginclassname$";
+
+        public IList<IWeatherData> GetEquipment() {
+            var devices = new List<IWeatherData>();
+            devices.Add(new $pluginclassname$WeatherDriver("MyUniqueId", "MyDeviceId"));
+
+            return devices;
+        }
+    }
+}
