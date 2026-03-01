6a8a84ae980f072dc64566b5e5aec9702fbc2539
Evan Pegors <edpegors@gmail.com>
2026-03-01 17:19:58 -0500

    Wire SessionCollector into NightSummaryPlugin

--- NINA.Plugin.Template/NightSummaryPlugin.cs
+++ NINA.Plugin.Template/NightSummaryPlugin.cs
@@ -1,135 +1,73 @@
-﻿using NINA.Plugin.NightSummary.Properties;
-using NINA.Core.Model;
-using NINA.Core.Utility;
-using NINA.Image.ImageData;
+﻿using NINA.Core.Utility;
 using NINA.Plugin;
 using NINA.Plugin.Interfaces;
-using NINA.Profile;
 using NINA.Profile.Interfaces;
 using NINA.WPF.Base.Interfaces.Mediator;
 using NINA.WPF.Base.Interfaces.ViewModel;
+using NINA.Plugin.NightSummary.Data;
+using NINA.Plugin.NightSummary.Session;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.Composition;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
 using System.Threading.Tasks;
-using Settings = NINA.Plugin.NightSummary.Properties.Settings;
 
 namespace NINA.Plugin.NightSummary {
-    /// <summary>
-    /// This class exports the IPluginManifest interface and will be used for the general plugin information and options
-    /// The base class "PluginBase" will populate all the necessary Manifest Meta Data out of the AssemblyInfo attributes. Please fill these accoringly
-    /// 
-    /// An instance of this class will be created and set as datacontext on the plugin options tab in N.I.N.A. to be able to configure global plugin settings
-    /// The user interface for the settings will be defined by a DataTemplate with the key having the naming convention "NightSummaryPlugin_Options" where NightSummaryPlugin corresponds to the AssemblyTitle - In this template example it is found in the Options.xaml
-    /// </summary>
     [Export(typeof(IPluginManifest))]
     public class NightSummaryPlugin : PluginBase, INotifyPropertyChanged {
-        private readonly IPluginOptionsAccessor pluginSettings;
+
         private readonly IProfileService profileService;
         private readonly IImageSaveMediator imageSaveMediator;
-
-        // Implementing a file pattern
-        private readonly ImagePattern exampleImagePattern = new ImagePattern("$$EXAMPLEPATTERN$$", "An example of an image pattern implementation", "Night Summary");
+        private readonly SessionDatabase database;
+        private readonly SessionCollector collector;
 
         [ImportingConstructor]
-        public NightSummaryPlugin(IProfileService profileService, IOptionsVM options, IImageSaveMediator imageSaveMediator) {
-            if (Settings.Default.UpdateSettings) {
-                Settings.Default.Upgrade();
-                Settings.Default.UpdateSettings = false;
-                CoreUtil.SaveSettings(Settings.Default);
-            }
+        public NightSummaryPlugin(
+            IProfileService profileService,
+            IOptionsVM options,
+            IImageSaveMediator imageSaveMediator) {
 
-            // This helper class can be used to store plugin settings that are dependent on the current profile
-            this.pluginSettings = new PluginOptionsAccessor(profileService, Guid.Parse(this.Identifier));
             this.profileService = profileService;
-            // React on a changed profile
-            profileService.ProfileChanged += ProfileService_ProfileChanged;
-
-            // Hook into image saving for adding FITS keywords or image file patterns
             this.imageSaveMediator = imageSaveMediator;
 
-            // Run these handlers when an image is being saved
-            this.imageSaveMediator.BeforeImageSaved += ImageSaveMediator_BeforeImageSaved;
-            this.imageSaveMediator.BeforeFinalizeImageSaved += ImageSaveMediator_BeforeFinalizeImageSaved;
+            // Initialize the database - creates SQLite file if it doesn't exist
+            this.database = new SessionDatabase();
 
-            // Register a new image file pattern for the Options > Imaging > File Patterns area
-            options.AddImagePattern(exampleImagePattern);
+            // Initialize the collector with the mediator and database
+            this.collector = new SessionCollector(imageSaveMediator, database);
+
+            Logger.Info("NightSummary: Plugin initialized successfully");
         }
 
         public override Task Teardown() {
-            // Make sure to unregister an event when the object is no longer in use. Otherwise garbage collection will be prevented.
-            profileService.ProfileChanged -= ProfileService_ProfileChanged;
-            imageSaveMediator.BeforeImageSaved -= ImageSaveMediator_BeforeImageSaved;
-            imageSaveMediator.BeforeFinalizeImageSaved -= ImageSaveMediator_BeforeFinalizeImageSaved;
-
+            // Make sure we cleanly end any active session when NINA shuts down
+            collector.EndSession();
+            Logger.Info("NightSummary: Plugin torn down");
             return base.Teardown();
         }
 
-        private void ProfileService_ProfileChanged(object sender, EventArgs e) {
-            // Rase the event that this profile specific value has been changed due to the profile switch
-            RaisePropertyChanged(nameof(ProfileSpecificNotificationMessage));
+        /// <summary>
+        /// Starts a new imaging session. This will be called from the
+        /// NightSummaryInstruction when added to a sequence.
+        /// </summary>
+        public void StartSession() {
+            var profileName = profileService?.ActiveProfile?.Name ?? "Unknown";
+            collector.StartSession(profileName);
         }
 
-        private Task ImageSaveMediator_BeforeImageSaved(object sender, BeforeImageSavedEventArgs e) {
-            // Insert the example FITS keyword of a specific data type into the image metadata object prior to the file being saved
-            // FITS keywords have a maximum of 8 characters. Comments are options. Comments that are too long will be truncated.
-
-            string exampleKeywordComment = "This is a {0} keyword";
-
-            // string
-            string exampleStringKeywordName = "STRKEYWD";
-            string exampleStringKeywordValue = "Example";
-            e.Image.MetaData.GenericHeaders.Add(new StringMetaDataHeader(exampleStringKeywordName, exampleStringKeywordValue, string.Format(exampleKeywordComment, "string")));
-
-            // integer
-            string exampleIntKeywordName = "INTKEYWD";
-            int exampleIntKeywordValue = 5;
-            e.Image.MetaData.GenericHeaders.Add(new IntMetaDataHeader(exampleIntKeywordName, exampleIntKeywordValue, string.Format(exampleKeywordComment, "integer")));
-
-            // double
-            string exampleDoubleKeywordName = "DBLKEYWD";
-            double exampleDoubleKeywordValue = 1.3d;
-            e.Image.MetaData.GenericHeaders.Add(new DoubleMetaDataHeader(exampleDoubleKeywordName, exampleDoubleKeywordValue, string.Format(exampleKeywordComment, "double")));
-
-            // Classes also exist for other data types:
-            // BoolMetaDataHeader()
-            // DateTimeMetaDataHeader()
-
-            return Task.CompletedTask;
+        /// <summary>
+        /// Ends the current session. This will be called from the
+        /// NightSummaryInstruction at sequence end.
+        /// </summary>
+        public void EndSession() {
+            collector.EndSession();
         }
 
-        private Task ImageSaveMediator_BeforeFinalizeImageSaved(object sender, BeforeFinalizeImageSavedEventArgs e) {
-            // Populate the example image pattern with data. This can provide data that may not be immediately available
-            e.AddImagePattern(new ImagePattern(exampleImagePattern.Key, exampleImagePattern.Description, exampleImagePattern.Category) {
-                Value = $"{DateTime.Now:yyyy-MM-ddTHH:mm:ss.ffffffK}"
-            });
-
-            return Task.CompletedTask;
-        }
-
-        public string DefaultNotificationMessage {
-            get {
-                return Settings.Default.DefaultNotificationMessage;
-            }
-            set {
-                Settings.Default.DefaultNotificationMessage = value;
-                CoreUtil.SaveSettings(Settings.Default);
-                RaisePropertyChanged();
-            }
-        }
-
-        public string ProfileSpecificNotificationMessage {
-            get {
-                return pluginSettings.GetValueString(nameof(ProfileSpecificNotificationMessage), string.Empty);
-            }
-            set {
-                pluginSettings.SetValueString(nameof(ProfileSpecificNotificationMessage), value);
-                RaisePropertyChanged();
-            }
+        /// <summary>
+        /// Returns the current session ID for use by the report generator.
+        /// </summary>
+        public string GetCurrentSessionId() {
+            return collector.GetCurrentSessionId();
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
