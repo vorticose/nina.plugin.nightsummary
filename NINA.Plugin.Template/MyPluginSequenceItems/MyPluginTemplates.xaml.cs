42a6235342d17e0fa8c424eee6ef642ac1627860
Evan Pegors <edpegors@gmail.com>
2026-03-01 17:39:50 -0500

    Fix XAML namespaces, successful .NET 8 build

--- NINA.Plugin.Template/MyPluginSequenceItems/MyPluginTemplates.xaml.cs
+++ NINA.Plugin.Template/MyPluginSequenceItems/MyPluginTemplates.xaml.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Accord.Statistics.Kernels;
 using System.ComponentModel.Composition;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 
-namespace NINA.Plugin.NightSummary.NightSummaryPluginTestCategory {
+namespace NINA.Plugin.NightSummary.Sequencer {
     [Export(typeof(ResourceDictionary))]
     public partial class PluginItemTemplate : ResourceDictionary {
         public PluginItemTemplate() {
