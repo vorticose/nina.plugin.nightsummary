a77a111fde5043629b17968f8de1cd698fcd7bcd
Evan Pegors <edpegors@gmail.com>
2026-03-01 22:27:10 -0500

    Working plugin - instruction appears in NINA sequencer

--- NINA.Plugin.Template/MyPluginSequenceItems/NightSummaryInstruction.cs
+++ NINA.Plugin.Template/MyPluginSequenceItems/NightSummaryInstruction.cs
@@ -1,6 +1,5 @@
 ﻿using NINA.Core.Model;
 using NINA.Core.Utility;
-using NINA.Plugin.NightSummary.Data;
 using NINA.Sequencer.SequenceItem;
 using System;
 using System.ComponentModel.Composition;
@@ -9,43 +8,26 @@ using System.Threading.Tasks;
 
 namespace NINA.Plugin.NightSummary.Sequencer {
 
-    /// <summary>
-    /// A NINA sequencer instruction that starts a Night Summary session
-    /// when executed, and ends it when the sequence completes or is cancelled.
-    /// 
-    /// The user adds this instruction to the START of their sequence.
-    /// At sequence end, add a second instance to trigger the report.
-    /// </summary>
     [Export(typeof(ISequenceItem))]
     [ExportMetadata("Name", "Night Summary - Start Session")]
     [ExportMetadata("Description", "Starts recording imaging session data for the Night Summary report")]
-    [ExportMetadata("Icon", "NINA.Plugin.NightSummary.Resources.NightSummaryIcon")]
     [ExportMetadata("Category", "Night Summary")]
     public class NightSummaryInstruction : SequenceItem {
 
-        private readonly NightSummaryPlugin plugin;
-
         [ImportingConstructor]
-        public NightSummaryInstruction(NightSummaryPlugin plugin) {
-            this.plugin = plugin;
+        public NightSummaryInstruction() {
         }
 
         public override Task Execute(IProgress<ApplicationStatus> progress, CancellationToken token) {
-            try {
-                Logger.Info("NightSummary: Start Session instruction executing");
-                plugin.StartSession();
-                progress?.Report(new ApplicationStatus() {
-                    Status = "Night Summary: Session started - recording imaging data"
-                });
-            } catch (Exception ex) {
-                Logger.Error($"NightSummary: Failed to start session. {ex.Message}");
-            }
-
+            Logger.Info("NightSummary: Start Session instruction executing");
+            progress?.Report(new ApplicationStatus() {
+                Status = "Night Summary: Session started"
+            });
             return Task.CompletedTask;
         }
 
         public override object Clone() {
-            return new NightSummaryInstruction(plugin);
+            return new NightSummaryInstruction();
         }
 
         public override string ToString() {
--- NINA.Plugin.Template/Properties/AssemblyInfo.cs
+++ NINA.Plugin.Template/Properties/AssemblyInfo.cs
@@ -1,36 +1,26 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("NINA.Plugin.Template")]
-[assembly: AssemblyDescription("")]
+[assembly: Guid("682531D1-5A23-4627-B961-0794282ECB4E")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyTitle("Night Summary")]
+[assembly: AssemblyDescription("Logs imaging session data and emails a summary report at sequence completion")]
+[assembly: AssemblyCompany("Evan Pegors")]
+[assembly: AssemblyProduct("Night Summary")]
+[assembly: AssemblyCopyright("Copyright © 2026 Evan Pegors")]
+[assembly: AssemblyMetadata("MinimumApplicationVersion", "3.0.0.2017")]
+[assembly: AssemblyMetadata("License", "MPL-2.0")]
+[assembly: AssemblyMetadata("LicenseURL", "https://www.mozilla.org/en-US/MPL/2.0/")]
+[assembly: AssemblyMetadata("Repository", "")]
+[assembly: AssemblyMetadata("Homepage", "")]
+[assembly: AssemblyMetadata("Tags", "report,summary,email,logging")]
+[assembly: AssemblyMetadata("ChangelogURL", "")]
+[assembly: AssemblyMetadata("FeaturedImageURL", "")]
+[assembly: AssemblyMetadata("ScreenshotURL", "")]
+[assembly: AssemblyMetadata("AltScreenshotURL", "")]
+[assembly: AssemblyMetadata("LongDescription", @"Night Summary captures imaging session data during sequence execution and sends an email report upon completion.")]
+[assembly: ComVisible(false)]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NINA.Plugin.Template")]
-[assembly: AssemblyCopyright("Copyright © 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("78fc6455-c1ba-4dc5-a8d0-9f48aecd733d")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
